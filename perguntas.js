6d04258d81146d347a653962301b648746f8af36
Xxscorpionx <00001125092105sp@al.educacao.sp.gov.br>
2024-11-26 08:14:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'qual o maio pais em extensão territorial do  mundo?',
+    'russia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz casa em ingles?',
+    'casa em ingles é house'
+)
